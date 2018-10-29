a7d890fae1680b788e68351264cba98ab918d302
longen0707 <1104777361@qq.com>
2018-10-29 18:28:54 +0800

    dd

--- Multilingual/mainwindow.cpp
+++ Multilingual/mainwindow.cpp
@@ -9,10 +9,12 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
           QTranslator *m_Translator=new QTranslator;
-          m_Translator->load("../Multilingual/simplify.qm");//设定初始语言问Chinese_Simplify
+          QSettings settings("GWT", "Language");
+          QString lang=settings.value("Language","simplify").toString();
+
+          m_Translator->load("../Multilingual/"+lang+".qm");
           qApp->installTranslator(m_Translator);
           ui->setupUi(this);
-
 }
 
 MainWindow::~MainWindow()
@@ -22,6 +24,8 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionChinese_Traditional_triggered()
 {
+    QSettings settings("GWT", "Language");
+    settings.setValue("Language","traditional");
     QTranslator *m_Translator=new QTranslator;
     m_Translator->load("../Multilingual/traditional.qm");
     qApp->installTranslator(m_Translator);
@@ -32,6 +36,8 @@ void MainWindow::on_actionChinese_Traditional_triggered()
 
 void MainWindow::on_actionChinese_triggered()
 {
+    QSettings settings("GWT", "Language");
+    settings.setValue("Language","simplify");
     QTranslator *m_Translator=new QTranslator;
     m_Translator->load("../Multilingual/simplify.qm");
     qApp->installTranslator(m_Translator);
@@ -42,6 +48,8 @@ void MainWindow::on_actionChinese_triggered()
 
 void MainWindow::on_actionJapanese_triggered()
 {
+    QSettings settings("GWT", "Language");
+    settings.setValue("Language","japanese");
     QTranslator *m_Translator=new QTranslator;
     m_Translator->load("../Multilingual/japanese.qm");
     qApp->installTranslator(m_Translator);
@@ -52,6 +60,8 @@ void MainWindow::on_actionJapanese_triggered()
 void MainWindow::on_actionKorean_triggered()
 {
     QTranslator *m_Translator=new QTranslator;
+    QSettings settings("GWT", "Language");
+    settings.setValue("Language","korean");
     m_Translator->load("../Multilingual/korean.qm");
     qApp->installTranslator(m_Translator);
 
@@ -61,6 +71,8 @@ void MainWindow::on_actionKorean_triggered()
 void MainWindow::on_actionFranch_triggered()
 {
     QTranslator *m_Translator=new QTranslator;
+    QSettings settings("GWT", "Language");
+    settings.setValue("Language","franch");
     m_Translator->load("../Multilingual/franch.qm");
     qApp->installTranslator(m_Translator);
 
@@ -70,6 +82,8 @@ void MainWindow::on_actionFranch_triggered()
 void MainWindow::on_actionEnglish_triggered()
 {
     QTranslator *m_Translator=new QTranslator;
+    QSettings settings("GWT", "Language");
+    settings.setValue("Language","english");
     m_Translator->load("../Multilingual/english.qm");
     qApp->installTranslator(m_Translator);
 
